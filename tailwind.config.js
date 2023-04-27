6d4c8ddab7783537bead691db3197d3f7c4a0087
Seb <seb@hostme.fr>
2023-04-27 17:40:33 +0200

    colors tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,23 @@ module.exports = {
             sans: ['Outfit', ...defaultTheme.fontFamily.sans],
           },
           backgroundColor: {
-            'header': '#0351B1',
-            'footer': '#032D6E',
             'main': '#F0F9FC'
           },
           colors: {
             'footer': '#96B2DB',
+            'blue': {
+              DEFAULT: '#0351B1',
+              '50': '#f2f6fb', 
+              '100': '#e6eef7', 
+              '200': '#c0d4ec', 
+              '300': '#9ab9e0', 
+              '400': '#4f85c8', 
+              '500': '#0351b1', 
+              '600': '#03499f', 
+              '700': '#023d85', 
+              '800': '#02316a', 
+              '900': '#032D6E'
+            },
           },
         },
     },
